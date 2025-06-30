7e15825667dc1d28a802c877ea07a84da015750a
Nacho Codoñer <igcogi@gmail.com>
2025-06-30 15:14:34 +0200

    mark dev modules as devOnly in package configurations

--- packages/static-html/package.js
+++ packages/static-html/package.js
@@ -3,7 +3,6 @@ Package.describe({
   summary: "Define static page content in .html files",
   version: '1.4.0',
   git: 'https://github.com/meteor/meteor.git',
-  devOnly: true,
 });
 
 Package.registerBuildPlugin({
