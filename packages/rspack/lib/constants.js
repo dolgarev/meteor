043ddd8536cfa48e559d6d9b3882f162b75cb98a
Nacho Codoñer <igcogi@gmail.com>
2025-09-04 15:01:41 +0200

    bump @meteorjs/rspack version 0.0.40

--- packages/rspack/lib/constants.js
+++ packages/rspack/lib/constants.js
@@ -5,7 +5,7 @@
 
 export const DEFAULT_RSPACK_VERSION = '1.5.0';
 
-export const DEFAULT_METEOR_RSPACK_VERSION = '0.0.39';
+export const DEFAULT_METEOR_RSPACK_VERSION = '0.0.40';
 
 export const DEFAULT_METEOR_RSPACK_REACT_HMR_VERSION = '1.4.3';
 
