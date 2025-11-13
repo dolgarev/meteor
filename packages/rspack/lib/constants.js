84521eb4705ea23ad285df5d5cde5bf702617aa4
Nacho Codoñer <igcogi@gmail.com>
2025-11-13 09:08:23 +0100

    update @meteorjs/rspack to version 0.1.3 across all packages

--- packages/rspack/lib/constants.js
+++ packages/rspack/lib/constants.js
@@ -5,7 +5,7 @@
 
 export const DEFAULT_RSPACK_VERSION = '1.6.0';
 
-export const DEFAULT_METEOR_RSPACK_VERSION = '0.1.2';
+export const DEFAULT_METEOR_RSPACK_VERSION = '0.1.3;
 
 export const DEFAULT_METEOR_RSPACK_REACT_HMR_VERSION = '1.4.3';
 
