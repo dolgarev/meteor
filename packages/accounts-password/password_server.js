2a6d28d44cdcbe0818c2972fb62c914c7226afeb
Jan Dvorak <jdvorak@literaryuniverse.com>
2025-12-17 21:28:38 +0100

    Do not throw error on forgotPassword when ambiguous message set #11336

--- packages/accounts-password/password_server.js
+++ packages/accounts-password/password_server.js
@@ -513,6 +513,7 @@ Meteor.methods({forgotPassword: async options => {
   const user = await Accounts.findUserByEmail(options.email, { fields: { emails: 1 } });
 
   if (!user) {
+    if (Accounts._options.ambiguousErrorMessages) return;
     Accounts._handleError("User not found");
   }
 
