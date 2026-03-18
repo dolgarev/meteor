875fe1d51e54028d172d3de1d325f2d6bcd33df5
Frederico Maia <fredmaiaarantes@gmail.com>
2026-03-18 15:17:18 -0300

    Add TypeScript + Tailwind skeleton support
    - Introduced a new skeleton type "typescript-tailwind".
    - Added necessary configuration files and directories for the new skeleton.
    - Updated documentation to reflect the new skeleton option in the CLI.

---
+++ tools/static-assets/skel-typescript-tailwind/imports/ui/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Hello } from './Hello';
+import { Info } from './Info';
+
+export const App = () => (
+  <div className="max-w-3xl min-h-screen mx-auto sm:pt-10">
+    <Hello/>
+    <Info/>
+  </div>
+);
